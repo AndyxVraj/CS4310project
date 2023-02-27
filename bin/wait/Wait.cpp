9e553b6965a65846d88c56b254cb1773a5c05c6b
apereza <pereza969800p@gmail.com>
2023-02-27 00:58:38 -0800

    working

--- bin/wait/Wait.cpp
+++ bin/wait/Wait.cpp
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
 #include <stdio.h>
@@ -21,12 +21,16 @@
 #include <errno.h>
 #include <unistd.h>
 #include "Wait.h"
+#include <sys/wait.h>
+#include <ProcessClient.h>
+
+
 
 Wait::Wait(int argc, char **argv)
     : POSIXApplication(argc, argv)
 {
-    parser().setDescription("Stop executing for some time");
-    parser().registerPositional("SECONDS", "Stop executing for the given number of seconds");
+    parser().setDescription("It suspends the calling process until a child process ends or is stopped.");
+    parser().registerPositional("PID", "Suspend execution until a child process ends or is stopped.");
 }
 
 Wait::~Wait()
@@ -35,9 +39,19 @@ Wait::~Wait()
 
 Wait::Result Wait::exec()
 {
-    int sec = 0;
-    const 
+    pid_t pid = atoi(arguments().get("PID"));
 
+    const ProcessClient process;
+    ProcessClient::Info info;
+
+
+    const ProcessClient::Result result = process.processInfo(pid, info);
+
+    if(waitpid(pid,0,0) == -1)
+    {
+        ERROR("Invalid PID " << strerror(errno));
+        return IOError;
+    }
 
     // Done
     return Success;
